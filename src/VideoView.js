921a30f61623d6eb25f434104b54cb7d92d0c527
Aleksander Misztal <apm81@cam.ac.uk>
2021-02-25 21:21:53 +0100

    Add video view controls

--- src/VideoView.js
+++ src/VideoView.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 class VideoView extends Component {
   title = 'Video View';
   video_source = 'http://127.0.0.1:5000/video';
+  state = { recording: false };
+
+  takePicture = () => {
+    console.log('Picture taken');
+  };
+
+  toggleRecording = () => {
+    this.setState({ recording: !this.state.recording });
+  };
 
   render = () => {
     return (
       <div>
-        VideoView Body
+        <button onClick={this.takePicture}>Take picture</button>
+        <button onClick={this.toggleRecording}>
+          {this.state.recording ? 'Stop recording' : 'Start recording'}
+        </button>
         <img src={this.video_source}></img>
       </div>
     );
